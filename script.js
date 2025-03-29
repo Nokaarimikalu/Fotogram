4a1b8d90d0dca050af351533990e22e168bb0b3a
Pascal Fliednr <fliednerpascal@gmail.com>
2025-03-29 14:56:52 +0100

    test

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 /** @format */
 
+/* <div id="main">
+<div class="child1">Image Name</div>
+<div class="child2"><img src="./gatze5.PNG" alt="" /></div>
+<div class="child3">
+    <img onclick="" src="left arrow.png" alt="" />
+    <span>1/2</span>
+    <img onclick="" src="right arrow.png" alt="" />
+</div>
+</div> */
+
 let image = [
     `./img/Bella.jpg`,
     `./img/Bella1.jpg`,
@@ -20,14 +30,34 @@ let image = [
 
 function render() {
     let templateimg = document.getElementById(`template_container`);
-
+    let pop_up = document.getElementById(`main_pop_up`);
     for (let i = 0; i < image.length; i++) {
         templateimg.innerHTML += getnotesHTML(i);
+    }
+    for (let i = 0; i < image.length; i++) {
+        if (i < 1) {
+            pop_up.innerHTML += pop_up_template(i);
+        }
     }
 }
 
 function getnotesHTML(index) {
     return `<div class="single_element">
-                <a href="#"><img src=${image[index]}></a>
+                <img onclick="toggle_pop_up()" src=${image[index]}>
             </div>`;
+}
+
+function pop_up_template(index) {
+    return `  <div class="child1">Image Name</div>
+                <div class="child2"><img src=${image[index]} alt="" /></div>
+                <div class="child3">
+                    <img onclick="" src="./img/left arrow.png" alt="" />
+                    <span>1/2</span>
+                    <img onclick="" src="./img/right arrow.png" alt="" />
+                </div>`;
+}
+
+function toggle_pop_up() {
+    let showornot = document.getElementById(`main_pop_up`);
+    showornot.classList.toggle(`d_none`);
 }
