62df3629313e8a3dcab22b25004f6e24b9197bca
Pascal Fliednr <fliednerpascal@gmail.com>
2025-03-31 07:58:53 +0200

    final ver 12

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let image = [
     `./img/gatze2.png`,
     `./img/gatze.jpg`,
     `./img/gatze7.png`,
-    `./img/gatze6.png`,
     `./img/gatze5.PNG`,
     `./img/gatze6.png`,
     `./img/mecanik.JPG`,
@@ -20,7 +19,7 @@ let image = [
 const cleanedImage = image.map((path) => path.replace(`./img/`, ""));
 const dNone = document.getElementById(`teplateimage`);
 const dim = document.getElementById(`dimming`);
-console.log(cleanedImage);
+let renderedimage = 0;
 
 function getout() {
     dNone.classList.toggle(`d_none`);
@@ -45,26 +44,35 @@ function toggle(index) {
     const toggled_none = dNone.classList.toggle(`d_none`);
     dim.classList.toggle(`d_none`);
 
+    renderedimage = index;
+
     if (!toggled_none) {
-        dNone.innerHTML = popuptemplate(index);
+        dNone.innerHTML = popupTemplate(index);
     } else {
         dNone.innerHTML = ``;
     }
 }
 
-function changeImage() {
-    if (i < image.length - 1) {
+function changeImage(next) {
+    renderedimage += next;
+
+    if (renderedimage < 0) {
+        renderedimage = image.length - 1;
+    } else if (renderedimage >= image.length) {
+        renderedimage = 0;
     }
+
+    dNone.innerHTML = popupTemplate(renderedimage);
 }
 
-function popuptemplate(i) {
+function popupTemplate(i) {
     return `<div id="popupimage">
                 <div class="child1">${cleanedImage[i]}</div>
                 <div class="child2"><img class="child2image" src="${image[i]}" alt="" /></div>
                 <div class="child3">
-                    <img class="leftright" onclick="" src="./img/left arrow.png" alt="" />
+                    <img class="leftright" onclick="changeImage(-1)" src="./img/left arrow.png" alt="" />
                     <span>${i + 1}/${image.length}</span>
-                    <img class="leftright" onclick="" src="./img/right arrow.png" alt="" />
+                    <img class="leftright" onclick="changeImage(1)" src="./img/right arrow.png" alt="" />
                 </div>
                 </div>`;
 }
